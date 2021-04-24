f0609eea1a576ed7f5e6a9148c6000fd183d9b67
dhanushya08 <81281117+dhanushya08@users.noreply.github.com>
2021-04-24 16:25:57 +0530

    Update train.py

--- model/train.py
+++ model/train.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import absolute_import
-__author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
+
 
 import tensorflow as tf
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 
 import sys
 
