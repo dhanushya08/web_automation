1d2edab5210aad1a7435ac51cf01fd73e6bb78d2
dhanushya08 <81281117+dhanushya08@users.noreply.github.com>
2021-05-27 16:11:36 +0530

    Update Sampler.py

--- model/classes/Sampler.py
+++ model/classes/Sampler.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from __future__ import absolute_import
-__author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
+
 
 from .Vocabulary import *
 from .BeamSearch import *
